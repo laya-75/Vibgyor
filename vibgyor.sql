d3012743cabeb0476f08bab507b389061977cae6
laya-75 <sidrallaya@gmail.com>
2026-01-20 16:29:44 +0530

    Make category_name and category_description not null

--- vibgyor.sql
+++ vibgyor.sql
@@ -2,9 +2,9 @@ create database vibgyor;
 use vibgyor;
 create table categories(
 category_id int auto_increment primary key,
-category_name varchar(100),
-category_description varchar(300),
+category_name varchar(100) not null,
+category_description varchar(300) not null,
 created_date timestamp default current_timestamp,
 updated_date timestamp default current_timestamp on update current_timestamp,
-status boolean default true
+category_status boolean not null
 );
